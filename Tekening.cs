194b6c4944968d44217c4b7c27f3c114b5d84157
Niels <nielsgroeneveld8@gmail.com>
2018-11-06 14:09:51 +0100

    added tekeningen

---
+++ Tekening.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchetsEditor
+{
+
+    class Tekening
+    {
+    }
+
+    class VierkantTekening : Tekening
+    {
+
+    }
+
+    class VolVierkantTekening : VierkantTekening
+    {
+
+    }
+
+    class LijnTekening : Tekening
+    {
+
+    }
+
+    class PenTekening : LijnTekening
+    {
+
+    }
+
+    class CirkelTekening : VierkantTekening
+    {
+
+    }
+
+    class VolCirkelTekening : CirkelTekening
+    {
+
+    }
+}
