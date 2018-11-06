cf3850b6f225cd299c1f3314859b7d0cc80f94a3
Niels <nielsgroeneveld8@gmail.com>
2018-11-06 14:19:50 +0100

    updated tekeningen

--- Tekening.cs
+++ Tekening.cs
@@ -1,42 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
 namespace SchetsEditor
 {
 
-    class Tekening
+    abstract class Tekening
     {
+        Point startpunt;
+
+        public abstract void Teken();
+    }
+
+    class TekstTekening : Tekening
+    {
+        public override void Teken()
+        {
+
+        }
     }
 
     class VierkantTekening : Tekening
     {
+        Point eindpunt;
 
+        public override void Teken()
+        {
+
+        }
     }
 
     class VolVierkantTekening : VierkantTekening
     {
+        public override void Teken()
+        {
 
+        }
     }
 
     class LijnTekening : Tekening
     {
+        public override void Teken()
+        {
 
+        }
     }
 
     class PenTekening : LijnTekening
     {
+        public override void Teken()
+        {
 
+        }
     }
 
     class CirkelTekening : VierkantTekening
     {
+        public override void Teken()
+        {
 
+        }
     }
 
     class VolCirkelTekening : CirkelTekening
     {
+        public override void Teken()
+        {
 
+        }
     }
 }
